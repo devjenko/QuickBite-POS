b0a4be23a18a3827751beff35ea15c1fba921254
devjenko <brandonrichardjenkins@gmail.com>
2025-12-13 15:10:35 +0700

    Project setup and initial commit

---
+++ data/products.ts
@@ -0,0 +1,7 @@
+import { Product } from "../types/product";
+
+export const products: Product[] = [
+  { id: "1", name: "Burger", price: 5.99, image: "" },
+  { id: "2", name: "Fries", price: 2.99, image: "" },
+  { id: "3", name: "Soda", price: 1.99, image: "" },
+];
