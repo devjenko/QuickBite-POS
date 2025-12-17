94829508e483ded58b04797c93bf1e846ba34b33
unknown <brandonrichardjenkins@gmail.com>
2025-12-17 10:48:36 +0700

    fix(CartSidebar): Typescript fix

--- components/ui/RightSidebarWrapper.tsx
+++ components/ui/RightSidebarWrapper.tsx
@@ -2,8 +2,8 @@ const RightSidebarWrapper = ({
   children,
   className,
 }: {
-  children: React.ReactNode;
-  className: string;
+  children?: React.ReactNode;
+  className?: string;
 }) => {
   return (
     <div className={`bg-[var(--White)] h-screen w-77.5 ${className}`}>
