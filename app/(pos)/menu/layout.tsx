f80ea1eb9faa9ee2b24a93c1cba70347bddfcce8
unknown <brandonrichardjenkins@gmail.com>
2025-12-22 10:36:42 +0700

    fix(auth/menu): Add redirect to /login to prevent unauthorized users from accessing the menu

--- app/(pos)/menu/layout.tsx
+++ app/(pos)/menu/layout.tsx
@@ -2,12 +2,16 @@ import CartSidebar from "@/components/sidebar/CartSidebar";
 import POSLayout from "@/components/layout/POSLayout";
 import MenuSidebar from "@/components/sidebar/MenuSidebar";
 import CenterContentContainer from "@/components/ui/CenterContentContainer";
-
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
 
-const MenuLayout = ({ children }: DashboardLayoutProps) => {
+const MenuLayout = async ({ children }: DashboardLayoutProps) => {
+  // redirect users that aren't authed
+  const session = await auth();
+  if (!session) redirect("/login");
   return (
     <POSLayout>
       <MenuSidebar />
