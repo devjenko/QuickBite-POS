a4037a74391e9adea8c4bc63fa46c9a292c25d55
unknown <brandonrichardjenkins@gmail.com>
2025-12-22 14:45:35 +0700

    refactor(ui/menu): Put loading skeleton that loads for each route in menu into its own component MenuSkeletonPage

---
+++ components/skeletons/MenuSkeletonPage.tsx
@@ -0,0 +1,14 @@
+import { SkeletonCard } from "@/components/ui/SkeletonCard";
+import CenterContentContainer from "@/components/ui/CenterContentContainer";
+
+const MenuSkeletonPage = () => {
+  return (
+    <CenterContentContainer className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-6 auto-rows-max">
+      {Array.from({ length: 14 }).map((_, i) => (
+        <SkeletonCard key={i} />
+      ))}
+    </CenterContentContainer>
+  );
+};
+
+export default MenuSkeletonPage;
