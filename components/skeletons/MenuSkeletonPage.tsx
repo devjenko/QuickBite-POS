1dfea50332683fec105f0c80faf27e3fa797aab2
devjenko <brandonrichardjenkins@gmail.com>
2025-12-27 15:21:22 +0700

    feat: add animate-pulse to skeleton card loaders

--- components/skeletons/MenuSkeletonPage.tsx
+++ components/skeletons/MenuSkeletonPage.tsx
@@ -4,7 +4,7 @@ import CenterContentContainer from "@/components/shared/CenterContentContainer";
 const MenuSkeletonPage = () => {
   return (
     <CenterContentContainer className="grid grid-cols-[repeat(auto-fill,minmax(250px,1fr))] gap-6 auto-rows-max">
-      <div className="col-span-full px-14 py-2.5 border rounded-sm h-12"></div>
+      <div className="col-span-full px-14 py-2.5 border rounded-sm h-12 animate-pulse"></div>
       {Array.from({ length: 18 }).map((_, i) => (
         <SkeletonCard key={i} />
       ))}
