af9d3e11056d6a6bf99c96b8bd13655613661ce0
devjenko <brandonrichardjenkins@gmail.com>
2026-01-03 23:42:33 +0700

    perf: Change font from fontsource to next/google font for better performance

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,15 @@ import type { Metadata } from "next";
 import "@/styles/globals.css";
 import Providers from "./providers";
 import { Toaster } from "sonner";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+  preload: true,
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "QuickBite",
@@ -18,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body className="font-[family-name:var(--font-inter)] bg-white antialiased overflow-hidden  max-w-750 m-auto">
         <Providers>{children}</Providers>
         <Toaster />
